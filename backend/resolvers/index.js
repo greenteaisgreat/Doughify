eb2d63d563967daa61fa4def98ff7d59dc4d12e6
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-13 17:22:43 -0500

    Add index.js File for All Merged Resolvers

---
+++ backend/resolvers/index.js
@@ -0,0 +1,11 @@
+import { mergeResolvers } from "@graphql-tools/merge";
+import userResolver from "./user.resolver";
+import transactionResolver from "./transaction.resolver";
+
+//this file is used to combine all resolvers into a single array for
+//ease of accessing. this approach helps with scaling, as any new
+//resolvers can be added into their own file and simply plugged in here
+
+const mergedResolvers = mergeResolvers([userResolver, transactionResolver]);
+
+export default mergedResolvers;
