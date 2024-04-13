2101c0edf85c6aa6529c97f137298947b4049c22
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-13 17:13:34 -0500

    Add userResolver for Query Type

---
+++ backend/resolvers/user.resolver.js
@@ -0,0 +1,11 @@
+//MUST use extension after filenames when importing (i.e., data.js)
+//this is due to ESM in combination with Node
+import { users } from "../dummyData/data.js";
+
+const userResolver = {
+  Query: {
+    users: () => users,
+  },
+};
+
+export default userResolver;
