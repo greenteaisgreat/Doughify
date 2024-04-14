06dbdb8b5422e910016b52d5af937b12dbed5457
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-13 21:37:14 -0500

    Add Comments to Justify Merging of Types

--- backend/typeDefs/index.js
+++ backend/typeDefs/index.js
@@ -2,7 +2,10 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import transactionTypeDef from "./transaction.typeDef.js";
 import userTypeDef from "./user.typeDef.js";
 
-//transaction and user typeDefs will be grouped together and exported from here
+//transaction and user typeDefs will be grouped together and exported from here;
+//it's beneficial to merge resolvers, type defs, etc. due to modularity of code.
+//if someone wanted to expand upon your code and/or collaborate, this allows
+//for easier integration, separation of concerns and even reusability
 const mergedTypeDefs = mergeTypeDefs([userTypeDef, transactionTypeDef]);
 
 export default mergedTypeDefs;
