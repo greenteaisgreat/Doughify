cba5d17646a2bea04fe615164f19b0934c5b3518
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-13 21:31:50 -0500

    Format Code Spacing for Readability

--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -5,7 +5,6 @@ import transactionResolver from "./transaction.resolver";
 //this file is used to combine all resolvers into a single array for
 //ease of accessing. this approach helps with scaling, as any new
 //resolvers can be added into their own file and simply plugged in here
-
 const mergedResolvers = mergeResolvers([userResolver, transactionResolver]);
 
 export default mergedResolvers;
