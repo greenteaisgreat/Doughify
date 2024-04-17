f07ca2678e776c7ddbb424d94a05042d601c9ffe
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-17 18:51:49 -0500

    Format Code Spacing for Readability

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -5,7 +5,6 @@ import { users } from "../dummyData/data.js";
 const userResolver = {
   Query: {
     users: () => users,
-
     user: (_, { userId }) => {
       return users.find((user) => user._id === userId);
     },
